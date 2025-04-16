2515ef4ff64d4ddc90ce557a684b95ce4cbc778f
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-16 13:27:53 +0300

    Auth servisi projesi eklendi

--- Mango.Services.AuthAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mango.Services.AuthAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
