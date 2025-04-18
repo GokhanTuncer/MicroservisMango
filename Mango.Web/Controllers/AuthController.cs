19c1003285f24ad2b8ed1c364f74854237658ecb
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-18 15:59:47 +0300

    Önyüze login ve register sayfalarının eklenmesi

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
@@ -25,6 +27,13 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer},
+            };
+
+            ViewBag.RoleList = roleList;
             return View();
         }
         public IActionResult Logout()
