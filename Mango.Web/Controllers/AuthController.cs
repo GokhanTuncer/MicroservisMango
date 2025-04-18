de608a438804aef120c0be6501d75b9362129055
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-18 16:55:57 +0300

    Register post authcontrollere eklendi

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Mango.Web.Utility;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
 {
@@ -35,6 +36,41 @@ namespace Mango.Web.Controllers
 
             ViewBag.RoleList = roleList;
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDTO obj)
+        {
+            ResponseDTO result = await _authService.RegisterAsync(obj);
+            ResponseDTO assignRole;
+
+            if (result != null && result.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            else
+            {
+                TempData["error"] = result.Message;
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer,Value=SD.RoleCustomer},
+            };
+
+            ViewBag.RoleList = roleList;
+            return View(obj);
         }
         public IActionResult Logout()
         {
