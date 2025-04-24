d18b66670e3f7621df6600170c8c883930cc734f
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-24 12:16:51 +0300

    Product APInin önyüzde consume edilmesi için eklemeler yapıldı

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -9,11 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
+
 builder.Services.AddHttpClient<ICouponService, CouponService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
+builder.Services.AddHttpClient<IProductService, ProductService>();
 
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
---
+++ Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDTO?> GetProduct(string productCode);
+        Task<ResponseDTO?> GetAllProductsAsync(); 
+        Task<ResponseDTO?> GetProductByIdAsync(int id);
+        Task<ResponseDTO?> CreateProductsAsync(ProductDTO productDTO);
+        Task<ResponseDTO?> UpdateProductsAsync(ProductDTO productDTO);
+        Task<ResponseDTO?> DeleteProductsAsync(int id);
+
+    }
+}
