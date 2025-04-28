bdcf4071ac56c84747d5fc4715087cb5d0bb922e
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-28 13:06:38 +0300

    Sepet api controlleri eklendi

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.PortableExecutable;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private readonly AppDBContext _db;
+        private ResponseDTO _response;
+        private IMapper _mapper;
+        public CartAPIController(AppDBContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDTO();
+        }
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDTO> CartUpsert(CartDTO cartDTO)
+        {
+            try
+            {
+                var cartHeaderFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserID == cartDTO.CartHeader.UserID);
+                if (cartHeaderFromDb == null)
+                {
+                    //Sepet başlığı yoksa yeniden oluştur
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDTO.CartHeader);
+                    _db.CartHeaders.Add(cartHeader);
+                    await _db.SaveChangesAsync();
+                    cartDTO.CartDetails.First().CartHeaderID = cartHeader.CartHeaderID;
+                    _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDTO.CartDetails.First()));
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    //Sepet başlığı varsa güncelle
+                    //Sepette aynı ürün var mı onu kontrol et
+                    var cartDetailsFromDb = await _db.CartDetails.FirstOrDefaultAsync(u => u.ProductID == cartDTO.CartDetails.First().ProductID && u.CartHeaderID == cartHeaderFromDb.CartHeaderID);
+
+                    if (cartDetailsFromDb == null)
+                    {
+                        cartDTO.CartDetails.First().CartHeaderID = cartHeaderFromDb.CartHeaderID;
+                        _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDTO.CartDetails.First()));
+                        await _db.SaveChangesAsync();
+
+                    }
+                    else
+                    {
+                        //Ürün sayısını güncelle
+                        cartDTO.CartDetails.First().Count += cartDetailsFromDb.Count;
+                        cartDTO.CartDetails.First().CartHeaderID += cartDetailsFromDb.CartHeaderID;
+                        cartDTO.CartDetails.First().CartDetailsID += cartDetailsFromDb.CartDetailsID;
+                        _db.CartDetails.Update(_mapper.Map<CartDetails>(cartDTO.CartDetails.First()));
+                        await _db.SaveChangesAsync();
+                    }
+                }
+                _response.Result = cartDTO;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();  
+            }
+            return _response;
+        }
+    }
+}
