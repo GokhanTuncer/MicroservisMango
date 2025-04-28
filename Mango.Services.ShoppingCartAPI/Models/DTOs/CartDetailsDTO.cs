9b1020b22e4162d83cbe959462cc75d096443a00
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-28 12:13:21 +0300

    Cart DTOları eklendi

---
+++ Mango.Services.ShoppingCartAPI/Models/DTOs/CartDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.DTOs
+{
+    public class CartDetailsDTO
+    {
+        public int CartDetailsID { get; set; }
+        public int CartHeaderID { get; set; }
+        public CartHeaderDTO? CartHeader { get; set; }
+        public int ProductID { get; set; }
+        public ProductDTO? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
