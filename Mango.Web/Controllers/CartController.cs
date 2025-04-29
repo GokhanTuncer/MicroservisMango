7fe63b25f23e7897097df83343c8157b5460b196
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-29 15:39:10 +0300

    Cart controller eklemesi

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
