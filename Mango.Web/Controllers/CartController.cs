1a2b506576e8dd936445d0f93336a8cf213dee45
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-03 13:28:26 +0300

    Giriş yapan kullanıcıya göre sepet getirme metodu controllere eklendi

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -1,6 +1,9 @@
-﻿using Mango.Web.Service.IService;
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
 {
@@ -16,7 +19,22 @@ namespace Mango.Web.Controllers
         [Authorize]
         public async Task<IActionResult> CartIndex()
         {
-            return View();
+            return View(await LoadCartDTOBasedOnLoggedInUser());
+        }
+
+        public async Task<CartDTO> LoadCartDTOBasedOnLoggedInUser()
+        {
+           var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub).FirstOrDefault()?.Value;
+            ResponseDTO? response = await _cartService.GetCartByUserIDAsync(userId);
+            if (response != null && response.IsSuccess)
+            {
+                CartDTO cartDTO = JsonConvert.DeserializeObject<CartDTO>(Convert.ToString(response.Result));
+                return cartDTO;
+            }
+            else
+            {
+                return new CartDTO();
+            }
         }
     }
 }
