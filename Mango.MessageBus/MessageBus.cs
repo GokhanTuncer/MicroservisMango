4be8c5cdf4a1a01585bec22200721c3a97e0d6a4
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-05 12:46:53 +0300

    messagebus classı eklenmesi

--- Mango.MessageBus/MessageBus.cs
+++ Mango.MessageBus/MessageBus.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Mango.MessageBus
 {
-    class MessageBus
+    public class MessageBus :  IMessageBus
     {
+        private string connectionString = "localhost(değiştir)";
+        public async Task PublishMessage(object message, string topic_queue_name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+            ServiceBusSender sender = client.CreateSender(topic_queue_name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalmessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+        }
     }
 }
